c1c12fa496c80aa77ef58c8cc06d6825a67cbde8
Aman-Mandal <002amankumar@gmail.com>
2023-08-30 21:41:04 +0530

    Started landing page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,26 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 const LandingPage = () => {
   return (
     <>
       <Head>
         <title>CrossX</title>
-        <meta name="description" content="multichain deployer" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name='description'
+          content='multichain deployer'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+        />
+        <link
+          rel='icon'
+          href='/favicon.ico'
+        />
       </Head>
+      <div>
+        <p>Hello there</p>
+      </div>
     </>
   );
 };
